ef3d287b976cbc9bf0f99b6f70b761ad7cccde58
Ethan Smith <ejsmith@suu.edu>
2019-04-05 12:02:41 -0600

    testing with lists

---
+++ lists/test.py
@@ -0,0 +1,12 @@
+def list():
+	x = 5
+	y =4
+	something = "something"
+	return [x,y,something];
+
+z = list()
+
+# I probably could have figured this out just by googling "Do lists work like arrays
+print (z[0])
+print (z[1])
+print (z[2])
