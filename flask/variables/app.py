d25b6056cc8225e2392da6a87665e1c6a8fd86d6
Ethan Smith <ejsmith@suu.edu>
2019-05-08 13:48:15 -0600

    added a test about http variables

---
+++ flask/variables/app.py
@@ -0,0 +1,39 @@
+from flask import Flask,request,redirect
+
+# test how variables can be passed around in POST requests, to possibly simplify how my webserver works
+#improving the webserver seems to be a common theme of this folder of test projects
+
+app = Flask(__name__)
+#url = "test"
+# basic test of reading argument variables
+light = 1
+@app.route("/a",methods = ['GET','POST'])
+def send():
+	if request.args.get('arg1') is None:
+		return "no arg1 provided"
+	return request.args.get('arg1')
+
+#More advanced implimentation
+@app.route("/",methods= ['GET','POST'])
+def vartest():
+	if request.args.get('arg1') is not None:
+		changelight(request.args.get('arg1'))
+	return '<html><body>' + str(light) + '<br><a href = "/?arg1=yes">Change light</a></body></html>'
+
+# this might be the crappiest code I've ever written lol
+def changelight(do):
+	global light
+	if do == 'yes':
+		if light == 1:
+			light = 0
+		else:
+			light = 1
+	print light
+	return
+# leftovers from the template
+@app.route("/<path:url>")
+def test2(url):
+	return "your url is " + url
+
+if (__name__ == "__main__"):
+	app.run()
