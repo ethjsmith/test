6141f09ae785584430860b5b9f3e3b041ea23c06
Ethan Smith <ejsmith@suu.edu>
2019-06-26 13:45:19 -0600

    changed everything so that it all works ( with a database and sqlalchemy )

--- flask/user/vanilla.py
+++ flask/user/vanilla.py
@@ -1,56 +1,36 @@
-import sqlite3, flask_login, hashlib, sqlalchemy
+import flask_login, hashlib
 #from flask import Flask,current_app,g
 from flask import Flask, request, render_template, redirect, url_for, flash
-from flask_login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUserMixin,
-                            confirm_login, fresh_login_required)
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin, AnonymousUserMixin, confirm_login, fresh_login_required
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
 
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
 
-#url = "test"
-class User(UserMixin):
-        def __init__(self,name,passwd,id,active=True):
-            self.name = name
-            self.id = id
-            self.active = active
-            self.passwd = passwd
-            self.authed = False
-
-        def is_active(self):
-			# check if is_active
-            return self.active
-        def is_user(self):
-            return False
-        def is_authenticated(self):
-            return authed
+    def __init__(self,name,password,email):
+        self.name = name
+        self.password = password
+        self.email = email
+    def is_active(self):
+        return True
+    def is_user(self):
+        return False
+    def is_authenticated(self):
+        return True
 
 class Anon(AnonymousUserMixin):
-	name = u"Anon"
+	name = u"Not Logged in"
 
-# temporary stuff
-USERS = {
-	1: User(u"ejsmith","password",1),
-	2: User(u"bob","memes",2),
-	3: User(u"test","yes",3),
-}
-USER_NAMES = dict((u.name, u) for u in USERS.values())
-
-#accounts2 = [
-#    ["ejsmith","password","0"],
-#    ["bob","memes","1"],
-#    ["test","yes","2"]
-#]
-
-#accounts = {
-#    1: User("ejsmith","password",0),
-#    2: User("bob","meme",1),
-#    3: User("test","yes",2),
-#}
-#account_names = dict()
-
-app = Flask(__name__)
-app.secret_key = "8888888888888888888888888888888888"
-
+app.secret_key = "x9fLx81af*x90xbfx03xfaBxfcxc9r)x84x8bxd1xcafxe92x08x99x1exee8x05nt"
 login_manager = LoginManager()
 login_manager.anonymous_user = Anon
 login_manager.login_view = "login"
@@ -59,14 +39,8 @@ login_manager.refresh_view = "reauth"
 
 @login_manager.user_loader
 def load_user(id):
-	return USERS.get(int(id))
-#    if accounts[id][2] == id:
-#        user = User(accounts[id][0],accounts[id][1],accounts[id][2])
-#        return user
-#    else:
-#        return
-#    return accounts.get(int(id))
-    #return accounts[id]
+    return User.query.get(int(id))
+
 login_manager.setup_app(app)
 
 @app.route("/")
@@ -79,22 +53,10 @@ def test2(url):
 	return "user:" + current_user.name + " & your url is " + url
 
 @app.route("/login", methods=["GET","POST"])
-# def login():
-#	if request.method == "POST" and "username" in request.form:
-#		username = request.form["username"]
-#		if username in USER_NAMES:
-#			remember = request.form.get("rememeber", "no") == "yes"
-#			if login_user(USER_NAMES[username], remember=remember):
-#				flash("logged in!")
-#				return redirect(request.args.get("next") or "/")
-#			else:
-#				flash("false creds")
-#		else:
-#			flash(u"invalid username")
-#	return render_template("login.html")
-# better login?
 def login():
     if request.method == "POST" and "username" in request.form:
+        user = User.query.filter_by(email=request.form["username"]).first()
+        print(user)
         username = request.form["username"]
         pas = request.form["password"]
         print(username)
@@ -103,28 +65,17 @@ def login():
         #usr.name = username
         #usr.id = 1
         #usr.passwd = pas
-        if username in USER_NAMES:
+        if username  == user.email:
             print("username is in USER_NAMES")
-            if pas  == USER_NAMES[username].passwd:
+            if pas == user.password:
+            #if pas  == USER_NAMES[username].password:
                 print("good password")
-                USER_NAMES[username].authed = True
-                if login_user(USER_NAMES[username]):
-                    print("successful login")
+                #USER_NAMES[username].authed = True
+                #user
+                if login_user(user,remember=True):
+                    print("successful login for " + user.name)
                     return redirect("/")
-
-
-        #if username in accounts:
-        #for users in accounts:
-        #    if username == users.name:
-        #        if pas == users.passwd:
-        #            login_user(users)
-        #            return redirect(request.args.get("next") or "/")
-        #        return "wrong password"
-        #return "wrong username"
-    #else:
-    #    return "bad login"
     return render_template("login.html")
-
 
 @app.route("/logout")
 @login_required
