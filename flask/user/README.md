8a5ac32f8cc6c79ccf1ed98bf9e31f0202e58a7f
Ethan Smith <ejsmith@suu.edu>
2019-07-19 14:03:50 -0600

    added 95(+/-20)% of desired functionality for upgrade

--- flask/user/README.md
+++ flask/user/README.md
@@ -1,7 +1,7 @@
 Flask user : a dynamic flask site with users, and a database to store both users and articles. (this is basically the staging/building area for a huge upgrade to rpi_lightserver)
 
 (must be python3)
-run `python vanilla.py` 
+run `python vanilla.py`
 
 required installs : Flask, flask_login, flask_sqlachemy
 
@@ -44,3 +44,15 @@ db.session.add(p1)
 db.session.commit()
 quit()
 ```
+<p> School is filled with requirements to do lots of things, so on this page I'll write about some of the cooler, or more interesting things that I think I've done to complete various assignments</p>
+		<p>I've used github for a long time, so most of my projects are up there, and I can link them as well, so that's pretty nice.</p>
+		<p> The first time I really felt like I went above and beyond was for my BYUI CS101 class. The professor 'taught' us python, by basically having us follow the Code Academy course, and then had us do a 'final project' of our choosing. Me and my cousin Jared Wrathall ( who was a senior at the time, and probably did most of the heavy lifting) made a simple text adventure game, where you move around a map made of X's, looking for enemies, and gaining loot, through randomized, pokemon style text based encounters. I felt pretty good about the project, until I looked at what other people were doing, and realized that we could have just made a basic calculator or something, but either way, it was fun to make, and I enjoyed going all out for a project, and that attitude has stuck with me for all my programming classes.
+		You can check out the source code <a href = 'https://github.com/ddrgrevious/textadventure'> Right here </a></p>
+		<p> The second 'big for it's time' project I did was in my BYUI CIT-160 javascript programming class. Our teacher showed us how to draw in a screen, and I realized that I could update that screen, by redrawing the screen (and discovering basic animation), and so I decided to use this technique to recreate the classic 'Pong' game in javascript.
+		It was really interesting for me, because it taught me a lot about game design, ( like how a game loop works, and variable scope), as well as how useful functions really are. and as a bonus the class was really impressed, because it was a very simple class, and most of the rest of the projects weren't even graphical in nature. I have that source code ( or most of it anyways) up on my github <a href='https://github.com/urd000med/urd000med_pong'> Right here </a></p>
+		<p> I wasn't really engaged in my first programming class at SUU, which I didn't think I should have even been in, but was anyway, because my adviser disagreed with me ( she was and still is wrong ), so when it was suggested that we would be working on a giant final project for half the semester I decided that I was going to make something crazy. This class had a similar scope to my last one, with nothing crazy going on , and so I decided to follow pong, with Asteroids. This ended up being quite a large commitment of time, and energy, as it was the largest project I had ever undertaken, and there was lots of objects working with each other, plus it was written in java, a language that, until that semester, I had no experience with.
+		The project ended up turning out pretty good, with the basic mechanics working fine, although there is more that I would have wanted to add, if I had more time, and energy, (like the asteroids splitting into smaller asteroids when hit, or a prettier loading screen ) <a href = 'https://github.com/urd000med/1400final'> You can view the java code here </a></p><br>
+		<p> In my 1410 class I ended up making a pretty standard final tower defence project (which is in a private github repo, so no code for now, sorry ) but I did go above and beyond in adding a lot of cool quality of life improvements, like when you kill an enemy it's corpse stays on the screen for awhile, and animated enemy movement. This was one of the first sometimes
+		that I really understood how complicated making crazy advanced games is, because I had to draw all the sprites myself, and I kept running into problems which I should have fixed in a parent class, but instead I tried to patch quickly in a child. I also saw how much math really can go into a complicated project. I tried to make a tower that shot lightning , by picking a random point about halfway between the target enemy,
+		and the firing tower. but to do that you need to use some complicated trigonometry. it was a really interesting, and eye opening experience trying to change numbers slightly to get the desired effect, and wishing I understood math a bit better.</p>
+		<p> Hopefully I'll have some more crazy projects to write about here later, but until then, this is is!</p>
--- flask/user/vanilla.py
+++ flask/user/vanilla.py
@@ -1,14 +1,14 @@
 
 import flask_login, hashlib
-#from flask import Flask,current_app,g
+#from flask import Flask,current_ap,g
 from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin, AnonymousUserMixin, confirm_login, fresh_login_required
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+ap = Flask(__name__)
+ap.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
+ap.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(ap)
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -62,7 +62,7 @@ class User(UserMixin, db.Model):
 class Anon(AnonymousUserMixin):
 	name = u"Not Logged in"
 
-app.secret_key = "x9fLx81af*x90xbfx03xfaBxfcxc9r)x84x8bxd1xcafxe92x08x99x1exee8x05nt"
+ap.secret_key = "x9fLx81af*x90xbfx03xfaBxfcxc9r)x84x8bxd1xcafxe92x08x99x1exee8x05nt"
 login_manager = LoginManager()
 login_manager.anonymous_user = Anon
 login_manager.login_view = "login"
@@ -72,7 +72,7 @@ login_manager.refresh_view = "reauth"
 def load_user(id):
     return User.query.get(int(id))
 
-login_manager.setup_app(app)
+login_manager.setup_app(ap)
 def get_topics():
     topics = Post.query.with_entities(Post.topic).distinct()
     return topics
@@ -87,17 +87,20 @@ def get_posts():
     z = Post.query.all()
     return z
 
-@app.route("/")
+@ap.route("/")
 def testfunc():
     z = "user:" + current_user.name
     if isinstance(current_user,Anon):
         z += "<br>is anon"
     return render_template('genericpage.html',title="home",body="Welcome to the homepage",topics=get_topics())
 
-
+@ap.route('/About')
+def about_page():
+    k = "<h1>About Me</h1><br><p>My name is Ethan Smith, and I am a CSIS student at Southern Utah University. at SUU I am also the Vice President of the cyber defence (competition) club, and a student security analyst. I love programming ( prefer Python and Java), Snowboarding during the winter, and playing lots of different video games. I also enjoy homemade IOT devices, and <br> Contact me at `ethan@esmithy.net` </p> <p> About the site: <br> This site was built as a project, just something that I like to play around with when I have some downtime between work and school. I had the idea to make a website which instead of having static html files and PHP templates, would use python to generate all the pages by chaining together string variables containing bits of html, which altogether would generate web pages. I've done a lot of things to try and make the site scalable, instead of static, and I've really enjoyed putting it together, although writing html with python syntax highlighting can be a pain sometimes! </p>"
+    return render_template('genericpage.html',title="About",body=k,topics=get_topics())
 	#return "user:" + current_user.name + " & your url is " + url
 
-@app.route('/register', methods=["GET","POST"])
+@ap.route('/register', methods=["GET","POST"])
 def register():
     if current_user.name != "Not Logged in":
         return redirect ("/mypage")
@@ -108,11 +111,12 @@ def register():
                 new = User(request.form['name'],request.form['password'],request.form['email'])
                 db.session.add(new)
                 db.session.commit()
-                return "new user added "
+                flash("New user added")
+                return redirect('/login')
     return render_template("register.html",topics=get_topics())
 
 
-@app.route("/login", methods=["GET","POST"])
+@ap.route("/login", methods=["GET","POST"])
 def login():
     if current_user.name != "Not Logged in":
         return redirect("/mypage")
@@ -138,14 +142,45 @@ def login():
         flash("login failed, wrong username(email) or password","alert")
     return render_template("login.html",topics=get_topics())
 
-@app.route("/logout")
+@ap.route("/logout")
 @login_required
 def logout():
 	logout_user()
 	flash("logged out")
 	return redirect("/")
 
-@app.route('/admin')
+@ap.route('/control')
+@login_required
+def control():
+    if is_admin():
+        return redirect("/mypage")
+    bdy = "<a href='/control/go?arg=on'>Light On</a><br><a href='/control/go?arg=off'>Light Off</a><br><a href='/control/go?arg=on1'>Fan On(This doesnt do anything)</a><br><a href='/control/go?arg=off1'>Fan Off( This ones doesn\'t either lol)</a><br></p>"
+    return render_template("genericpage.html",body=bdy,topics=get_topics())
+
+@ap.route('/control/go')
+@login_required
+def doEverything():
+    if is_admin():
+        return redirect("/mypage")
+    if request.args.get('arg') != None:
+        if request.args.get('arg') == "on":
+            print("do thing 1")
+        elif request.args.get('arg') == "off":
+            print("do thing 2")
+        elif request.args.get('arg') == "on1":
+            print("do thing 3")
+        elif request.args.get('arg') == "off1":
+            print("do thing 4")
+        else:
+            print ("error?")
+    return redirect('/control')
+            #subprocess.call(['/usr/local/bin/rpi-rf_send',conf[0],conf[1]])
+    		#subprocess.call(['/usr/local/bin/rpi-rf_send',conf[0],conf[2]])
+			#subprocess.call(['/usr/local/bin/rpi-rf_send',conf[0],conf[3]])
+			#subprocess.call(['/usr/local/bin/rpi-rf_send',conf[0],conf[4]])
+
+
+@ap.route('/admin')
 @login_required
 def admin():
     if is_admin():
@@ -154,7 +189,7 @@ def admin():
     users = User.query.all()
     return render_template("admin.html",title='admin',topics=get_topics(),users=users,pages=get_posts())
 
-@app.route('/admin/du/<path:user>')
+@ap.route('/admin/du/<path:user>')
 @login_required
 def admin_delete_user(user):
     if is_admin():
@@ -166,7 +201,7 @@ def admin_delete_user(user):
     db.session.commit()
     return redirect('/admin')
 
-@app.route('/admin/da/<path:page>')
+@ap.route('/admin/da/<path:page>')
 @login_required
 def admin_delete_page(page):
     if is_admin():
@@ -176,14 +211,14 @@ def admin_delete_page(page):
     return redirect('/admin')
 
 
-@app.route('/mypage')
+@ap.route('/mypage')
 @login_required
 def userpage():
     return render_template("genericpage.html", body=current_user.name +":"+ current_user.email + "This is where you will be able to change your details bruh",topics=get_topics())
     # a form for changing uname and/or pword
     z = ""
 
-@app.route("/<path:url>")
+@ap.route("/<path:url>")
 def topic(url):
     print(request.path)
     #user = User.query.filter_by(email=request.form["username"]).first()
@@ -192,21 +227,21 @@ def topic(url):
         x = ""
         for post in posts:
             x += post.title + ":" + str(post.id) + "<br>"
-        return render_template("list.html",body = x,title = url, topics = get_topics(), articles = posts)
+        return render_template("list.html",title = url, topics = get_topics(), articles = posts)
         #return render_template("genericpage.html", body = x,title = url,topics=get_topics())
     return render_template("genericpage.html",body="Topic not found!",title="Error",topics=get_topics())
 #    return render_template('genericpage.html',title=url,body=url)
-@app.route("/<path:url>/<path:url2>")
+@ap.route("/<path:url>/<path:url2>")
 def artcle(url,url2):
     post = Post.query.filter_by(topic=url,id=url2).first()
     if post:
-        return render_template("article.html",art = post,topics=get_topics())
+        return render_template("article.html",art = post,topics=get_topics(),title=post.title)
     return render_template("genericpage.html",body="Article not found!",title="Error",topics=get_topics())
 
 
 
 if (__name__ == "__main__"):
-	app.run()
+	ap.run()
 
 #return '<html>' + stylesheet + templ.header(0) +
 #'<h1> File share </h1> <div class = \"card\">
