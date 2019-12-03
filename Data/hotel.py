b047127d4b886e3c561c6c920989f7deb02307a2
Ethan Smith <ejsmith@suu.edu>
2019-12-03 10:47:28 -0700

    why do I always forget the final commit :/

--- Data/hotel.py
+++ Data/hotel.py
@@ -1,6 +1,6 @@
 #Author Ethan Smith
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import mysql.connector
 
 db = mysql.connector.connect(
@@ -14,9 +14,43 @@ db = mysql.connector.connect(
 app = Flask(__name__)
 print(db)#debugging
 c = db.cursor()
+# a method that will generate the data of generic pages
+def genpage(ttable, tatribute=1,tvalue=1):
+    c.execute("describe " + ttable)
+    z = c.fetchall()
+    tableheadings = []
+    for x in z:
+        tableheadings.append(x[0])
+    c.execute("SELECT * FROM " + ttable + " where " +tatribute+ " = " + tvalue + ";")
+    content = c.fetchall()
+    return content,tableheadings
+
 @app.route("/")
 def homepage():
     return render_template("main.html")
+
+
+@app.data("/search", methods=["GET","POST"])
+def search():
+    if request.method =="POST":
+        c.execute("describe " + request.form['table'])
+        z = c.fetchall()
+        tableheadings = []
+        for x in z:
+            tableheadings.append(x[0])
+        c.execute("SELECT * FROM " + request.form['table'] + " where " +request.form['atribute']+ " = " + request.form['value'] + ";")
+        content = c.fetchall()
+
+        return render_template('table.html',tableheadings=tableheadings,content=content)
+    else:
+        return render_template("search.html")
+# required : show hotels and their managers
+@app.data("/hotelManager")
+def hotMan():
+    return render_template("main.html")
+
+
+
 # requried : show hotels
 @app.route("/hotels")
 def data():
