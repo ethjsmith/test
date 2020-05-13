0f18dc61f33bc2e726a5ddf728436110208396dd
Ethan Smith <ejsmith@suu.edu>
2020-05-13 13:52:47 -0600

    minor progress

--- flask/dnd/app.py
+++ flask/dnd/app.py
@@ -8,7 +8,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'zzz'
 db = SQLAlchemy(app)
 db.create_all()
-
+character = None
 class Character(db.Model):
 	__tablename__ = "Character"
 	id = db.Column(db.Integer, primary_key=True)
@@ -28,24 +28,59 @@ class Spell(db.Model):
 	spellname = db.Column(db.String())
 	cooldown = db.Column(db.Integer())
 	current = db.Column(db.Integer())
-	def __init__(self,sname,coold):
+	def __init__(self,sname,coold,pid):
 		self.spellname = sname
 		self.cooldown = coold
 		self.current = 0
+		self.parent_id = pid
 	# This should happen at the start of a turn :^) because that's how I think it should be
 	def dec(self):
 		if self.current > 0:
 			self.current -= 1
 	def cast(self):
 		self.current = self.current + self.cooldown
-		print(self.current)
-
+# gives templates access to all stuff :)
+@app.context_processor
+def giveFunctions():
+	def getCharacters():
+		charz = Character.query.all()
+		return charz
+	def getSpells(id=None):
+		if id is not None:
+			Spel = Spell.query.filter_by(parent_id=id).all()
+		else:
+			Spel = Spell.query.all()
+		return Spel
+	return dict(getCharacters=getCharacters,getSpells=getSpells)
+# check to make sure you have a character selected when you try to interact with the spells 
+def char_selected(f):
+	def wrap(*args, **kwargs):
+		if current_char != None:
+			return f(*args, **kwargs)
+		else:
+			return redirect("/char")
+@char_selected
 @app.route("/")
+def tfun():
+	chars = Character.query.all()
+	for char in chars:
+		print(char.name + ": " + str(char.id))
+		spel = Spell.query.filter_by(parent_id=char.id).all()
+		for spe in spel:
+			print(spe.spellname)
+		print("break :)")
+	return "dontLookHere :^)"
+@app.route("/a")
 def testfunc():
 	spells = Spell.query.all()
-	for s in spells:
-		print(s.current)
 	return render_template("spellpage.html", spells=spells)
+@app.route("/char", methods = ["GET","POST"])
+def char_select():
+	if request.method == "POST":
+		character = request.form['character']
+		return redirect("/")
+	return render_template("chars.html"chars=Character.query.all())
+
 
 @app.route("/cast/<path:spell>")
 def castme(spell):
