a058615a6cf17d86ac2dd3c7440be25e4001921d
Ethan Smith <ejsmith@suu.edu>
2020-05-15 13:24:36 -0600

    v 1.0 complete ?

--- flask/dnd/app.py
+++ flask/dnd/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect
+from flask import Flask,render_template, redirect,g, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 #url = "test"
@@ -8,7 +8,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'zzz'
 db = SQLAlchemy(app)
 db.create_all()
-character = None
+#session['char'] = None
 class Character(db.Model):
 	__tablename__ = "Character"
 	id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +30,7 @@ class Spell(db.Model):
 	current = db.Column(db.Integer())
 	def __init__(self,sname,coold,pid):
 		self.spellname = sname
-		self.cooldown = coold
+		self.cooldown = coold + 1
 		self.current = 0
 		self.parent_id = pid
 	# This should happen at the start of a turn :^) because that's how I think it should be
@@ -39,6 +39,29 @@ class Spell(db.Model):
 			self.current -= 1
 	def cast(self):
 		self.current = self.current + self.cooldown
+
+
+# check to make sure you have a character selected when you try to interact with the spells
+def char_selected(f):
+#	@wraps(f)
+	def wrap(*args, **kwargs):
+		if g.character != None:
+			return f(*args, **kwargs)
+		else:
+			return redirect("/char")
+	wrap.__name__=f.__name__
+	return wrap
+
+@app.before_request
+def before():
+    """Sets up variables before each function runs"""
+    g.character = None
+    g.charname = "None Selected"
+    if 'character' in session:
+        g.character = session['character']
+        g.charname = Character.query.filter_by(id=g.character).first()
+        #g.charname = x.name
+
 # gives templates access to all stuff :)
 @app.context_processor
 def giveFunctions():
@@ -52,15 +75,8 @@ def giveFunctions():
 			Spel = Spell.query.all()
 		return Spel
 	return dict(getCharacters=getCharacters,getSpells=getSpells)
-# check to make sure you have a character selected when you try to interact with the spells 
-def char_selected(f):
-	def wrap(*args, **kwargs):
-		if current_char != None:
-			return f(*args, **kwargs)
-		else:
-			return redirect("/char")
-@char_selected
-@app.route("/")
+
+@app.route("/a")
 def tfun():
 	chars = Character.query.all()
 	for char in chars:
@@ -68,21 +84,36 @@ def tfun():
 		spel = Spell.query.filter_by(parent_id=char.id).all()
 		for spe in spel:
 			print(spe.spellname)
-		print("break :)")
-	return "dontLookHere :^)"
-@app.route("/a")
+		print("--------------")
+	return "Go check the logs here :) "
+
+@app.route("/", methods = ["GET","POST"])
+@char_selected
 def testfunc():
-	spells = Spell.query.all()
+	if request.method == "POST" and 'spellname' in request.form and 'level' in request.form:
+		a = Spell(request.form["spellname"],request.form["level"],g.character)
+		db.session.add(a)
+		db.session.commit()
+	spells = Spell.query.filter_by(parent_id = g.character).all()
 	return render_template("spellpage.html", spells=spells)
+
 @app.route("/char", methods = ["GET","POST"])
 def char_select():
 	if request.method == "POST":
-		character = request.form['character']
+		if 'uname' in request.form and 'cname' in request.form:
+			a = Character(request.form['uname'],request.form['cname'])
+			db.session.add(a)
+			db.session.commit()
+			zz = Character.query.filter_by(name=request.form['uname']).first()
+			session['character'] = zz.id
+		else:
+			session["character"] = request.form['character']
 		return redirect("/")
-	return render_template("chars.html"chars=Character.query.all())
+	return render_template("chars.html", chars=Character.query.all())
 
 
 @app.route("/cast/<path:spell>")
+@char_selected
 def castme(spell):
 	casted = Spell.query.filter_by(id=spell).first()
 	if casted.current == 0:
@@ -91,15 +122,14 @@ def castme(spell):
 	return redirect("/")
 
 @app.route("/takeTurn")
+@char_selected
 def do():
-	spells = Spell.query.all()
+	spells = Spell.query.filter_by(parent_id = g.character).all()
 	for spell in spells:
 		spell.dec()
 	db.session.commit()
 	return redirect("/")
-@app.route("/<path:url>")
-def test2(url):
-	return "your url is " + url
+
 
 if (__name__ == "__main__"):
 	app.run()
